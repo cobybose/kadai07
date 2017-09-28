f0b52f766a1d898e987452d13d0b9c77003b4265
cobybose <cobybose@gmail.com>
2017-09-28 22:30:50 +0900

    最新。しかし、syntax errorが結局解決できず。。

--- input_data.php
+++ input_data.php
@@ -1,88 +1,62 @@
 <?php
 
+//2. DB接続
+try {
+  $pdo = new PDO('mysql:dbname=gs_db31;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('DbConnectError:'.$e->getMessage());
+}
 
 
+//３．SQLを作って実行
+$stmt = $pdo->prepare("INSERT INTO gs_ac_table(id, indate, salary, bonus,
+other_rev, sum_in, food, utility, trans_net, housing, daily, social, other_exp, sum_out, total)VALUES(NULL, sysdate(), :salary, :bonus, :other_rev, :sum_in, :food, :utility, :trans_net, :housing, :daily, :social, :other_exp, :sum_out, :total)");
 
+$query = "SELECT SUM(sum_in),SUM(sum_out) FROM gs_ac_table";
+$stmt3 = $pdo->prepare($query);
+$stmt3->execute();
+$row = $stmt3->fetch(PDO::FETCH_ASSOC);
+
+$A_sum = $row['SUM(sum_in)'];
+$B_sum = $row['SUM(sum_out)'];
+$total = $A_sum - $B_sum;
+$total_num = number_format($total);
 
 ?>
 
-    <!DOCTYPE html>
-    <html lang="ja">
+<!DOCTYPE html>
+<html lang="ja">
 
     <head>
         <meta charset="utf-8">
-        <title>Document</title>
+        <title>家計簿</title>
         <link rel="stylesheet" href="css/style.css">
     </head>
 
     <body>
         <h1>家計簿</h1>
-        <h2>Income</h2>
         <form action="output_data.php" method="post">
-            <p>
-<!--
-                <select name="income1">
-                <option value="salary">給料</option>
-                <option value="gamble">ギャンブル収入</option>
-                <option value="pickup">拾った</option>
-                <option value="lottery">宝くじ</option>
-            </select>
--->
-                給料：<input type="text" name="salary">円</p>
-            <p>
-<!--
-                <select name="income2">
-                <option value="salary">給料</option>
-                <option value="gamble">ギャンブル収入</option>
-                <option value="pickup">拾った</option>
-                <option value="lottery">宝くじ</option>
-            </select>
--->
-                ボーナス：<input type="text" name="bonus">円</p>
-            <p>
-<!--
-                <select name="income3">
-                <option value="salary">給料</option>
-                <option value="gamble">ギャンブル収入</option>
-                <option value="pickup">拾った</option>
-                <option value="lottery">宝くじ</option>
-            </select>
--->
-                雑収入：<input type="text" name="other_rev">円</p>
-            
-<!--
-            <h2>Outcome</h2>
-            <p><select name="outcome1">
-                <option value="10-20">外食</option>
-                <option value="20-30">洋服</option>
-                <option value="30-40">デート</option>
-                <option value="40-">日用品</option>
-            </select>：<input type="text" name="name">円</p>
-            <p><select name="outcome1">
-                <option value="10-20">外食</option>
-                <option value="20-30">洋服</option>
-                <option value="30-40">デート</option>
-                <option value="40-">日用品</option>
-            </select>：<input type="text" name="name">円</p>
-            <p><select name="outcome1">
-                <option value="10-20">外食</option>
-                <option value="20-30">洋服</option>
-                <option value="30-40">デート</option>
-                <option value="40-">日用品</option>
-            </select>：<input type="text" name="name">円</p>
--->
-            <h2>Outcome</h2>
-            食費：<input type="text" name="food">円</p>
-            水道光熱費：<input type="text" name="utility">円</p>
-            通信費・交通費：<input type="text" name="trans_net">円</p>
-            住居費：<input type="text" name="housing">円</p>
-            日用品費：<input type="text" name="daily">円</p>
-            交際費：<input type="text" name="social">円</p>
-            雑費：<input type="text" name="other_exp">円</p>
-
-            <input type="submit" value="送信">
+        <input type="text" name="date">
+        <p>繰り越し額：<input type="hidden" name="carryover" value="<?=$total;?>"><?=$total_num;?>円</p>
+        <div class="wrap_in">
+            <h2>収入</h2>
+            <div class="wrap"><div class="title">給料：</div><div><input type="text" name="salary">円</div></div>
+            <div class="wrap"><div class="title">ボーナス：</div><div><input type="text" name="bonus">円</div></div>
+            <div class="wrap"><div class="title">雑収入：</div><div><input type="text" name="other_rev">円</div></div>
+        </div>
+        <div class="wrap_out">
+            <h2>支出</h2>
+            <div class="wrap"><div class="title">食費：</div><div><input type="text" name="food">円</div></div>
+            <div class="wrap"><div class="title">水道光熱費：</div><div><input type="text" name="utility">円</div></div>
+            <div class="wrap"><div class="title">通信費・交通費：</div><div><input type="text" name="trans_net">円</div></div>
+            <div class="wrap"><div class="title">住居費：</div><div><input type="text" name="housing">円</div></div>
+            <div class="wrap"><div class="title">日用品費：</div><div><input type="text" name="daily">円</div></div>
+            <div class="wrap"><div class="title">交際費：</div><div><input type="text" name="social">円</div></div>
+            <div class="wrap"><div class="title">雑費：</div><div><input type="text" name="other_exp">円</div></div>
+        </div>
+            <input class="button" type="submit" value="送信">
         </form>
 
     </body>
 
-    </html>
+</html>
--- output_data.php
+++ output_data.php
@@ -1,17 +1,22 @@
 <?php
 
 //1. POST受信
+$date      = $_POST["date"];
 $salary    = $_POST["salary"];
 $bonus     = $_POST["bonus"];
 $other_rev = $_POST["other_rev"];
-$food = $_POST["food"];
-$utility = $_POST["utility"];
+$food      = $_POST["food"];
+$utility   = $_POST["utility"];
 $trans_net = $_POST["trans_net"];
-$housing = $_POST["housing"];
-$daily = $_POST["daily"];
-$social = $_POST["social"];
+$housing   = $_POST["housing"];
+$daily     = $_POST["daily"];
+$social    = $_POST["social"];
 $other_exp = $_POST["other_exp"];
+$carryover = $_POST["carryover"];
 
+$sum_in    = $salary + $bonus + $other_rev;
+$sum_out   = $food + $utility + $trans_net + $housing + $daily + $social + $other_exp;
+$total     = $carryover + $sum_in - $sum_out;
 
 //2. DB接続
 try {
@@ -22,11 +27,13 @@ try {
 
 
 //３．SQLを作って実行
-$stmt = $pdo->prepare("INSERT INTO gs_ac_table(id, indate, salary, bonus,
-other_rev, food, utility, trans_net, housing, daily, social, other_exp)VALUES(NULL, sysdate(), :salary, :bonus, :other_rev, :food, :utility, :trans_net, :housing, :daily, :social, :other_exp)");
+$stmt = $pdo->prepare("INSERT INTO gs_ac_table(id, date, salary, bonus,
+other_rev, sum_in, food, utility, trans_net, housing, daily, social, other_exp, sum_out, total)VALUES(NULL, :date, :salary, :bonus, :other_rev, :sum_in, :food, :utility, :trans_net, :housing, :daily, :social, :other_exp, :sum_out, :total)");
 $stmt->bindValue(':salary',   $salary,   PDO::PARAM_INT); //PARAM_STR or PARAM_INTを書く。（セキュリティ高くするもの）
+$stmt->bindValue(':date',  $date,  PDO::PARAM_STR);
 $stmt->bindValue(':bonus',  $bonus,  PDO::PARAM_INT);
 $stmt->bindValue(':other_rev', $other_rev, PDO::PARAM_INT);
+$stmt->bindValue(':sum_in', $sum_in, PDO::PARAM_INT);
 $stmt->bindValue(':food', $food, PDO::PARAM_INT);
 $stmt->bindValue(':utility', $utility, PDO::PARAM_INT);
 $stmt->bindValue(':trans_net', $trans_net, PDO::PARAM_INT);
@@ -34,19 +41,28 @@ $stmt->bindValue(':housing', $housing, PDO::PARAM_INT);
 $stmt->bindValue(':daily', $daily, PDO::PARAM_INT);
 $stmt->bindValue(':social', $social, PDO::PARAM_INT);
 $stmt->bindValue(':other_exp', $other_exp, PDO::PARAM_INT);
+$stmt->bindValue(':sum_out', $sum_out, PDO::PARAM_INT);
+$stmt->bindValue(':total', $total, PDO::PARAM_INT);
 $status = $stmt->execute();//errorになったら、$statusにfalseが入ってくる。
 
+//３．SQLを作って実行
+$stmt2 = $pdo->prepare("SELECT * FROM gs_ac_table ORDER BY id DESC");
+$status2 = $stmt2->execute();//errorになったら、$statusにfalseが入ってくる。
+
 //４．
-if($status==false){
-  $error = $stmt->errorInfo();
+$view = "";//loopの外に変数を作っておく
+if($status2==false){
+  $error = $stmt2->errorInfo();
   exit("QueryError:".$error[2]);//error文が３つ帰ってくるが、2つ目が英語の文になっているため。
   
 }else{
-  header("Location: input_data.php");
-  exit;
+    while($r = $stmt2->fetch(PDO::FETCH_ASSOC)){
+        $view .= '<p>';
+        $view .= $r["date"]."　".$r["sum_in"]."　".$r["sum_out"]."　".$r["total"];
+        $view .= '</p>';
 
+    }
 }
-
 
 ?>
     <!DOCTYPE html>
@@ -55,12 +71,45 @@ if($status==false){
     <head>
         <meta charset="utf-8">
         <title>Document</title>
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="../kadai07/css/style.css">
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
     </head>
 
     <body>
+        <?php
+        $con = mysql_connect('localhost', 'root', '');
 
+        mysql_select_db("gs_ac_table", $con);
+        $quryset = mysql_query('SET NAMES utf8', $con);
+        $quryset = mysql_query("SELECT * FROM gs_ac_table");
 
+        echo "<div id='graph'></div>";
+        echo "<script>";
+        echo "var day_data = [";
+        
+        while ($data = mysql_fetch_array($quryset)){
+                echo "{'DateT': '" . $data[1] . ", 'MinT':" .$data[13] . "},";
+        }
+                echo "];";
+                echo "Morris.Line({";
+                echo "element: 'graph',";
+                echo "data: day_data,";
+                echo "xkey: 'DateT',";
+//                echo "ymax: '150000',";
+//                echo "ymin: '-5',";
+                echo "ykeys: ['MinT'],";
+                echo "labels: ['最高気温'],";
+                echo "smooth: false,";
+                echo "resize: true";
+                echo "});";
+                echo "</script>";
+
+        ?>
+            <?=$view;?>
+        
     </body>
 
     </html>
